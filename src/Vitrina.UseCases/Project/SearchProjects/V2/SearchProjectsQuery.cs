3c200493cb62c6be1c4bbf5bc262f251812b4844
Nikita Kolchin <n.kolchin@smartway.today>
2024-12-28 15:30:54 +0300

    ad new filters

---
+++ src/Vitrina.UseCases/Project/SearchProjects/V2/SearchProjectsQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Saritasa.Tools.Common.Pagination;
+using Vitrina.UseCases.Common.Pagination;
+
+namespace Vitrina.UseCases.Project.SearchProjects.V2;
+
+/// <summary>
+/// Search projects.
+/// </summary>
+public class SearchProjectsQuery : PageQueryFilter, IRequest<PagedList<ShortProjectDto>>
+{
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public string? Name { get; init; }
+
+    /// <summary>
+    /// Period.
+    /// </summary>
+    public string? Client { get; init; }
+
+    /// <summary>
+    /// Organization.
+    /// </summary>
+    public string? Type { get; init; }
+
+    /// <summary>
+    /// Semester.
+    /// </summary>
+    public string? Sphere { get; init; }
+}
