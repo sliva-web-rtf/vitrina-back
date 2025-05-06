c426d8d027160dbf8982341aaa7f36db039299e1
stepan <stepan.zakolyukin@yandex.ru>
2025-05-06 20:17:05 +0500

    Added a database migration

---
+++ src/Vitrina.Infrastructure.DataAccess/Migrations/20250505175804_SmallChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vitrina.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SmallChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfileData",
+                table: "AspNetUsers",
+                newName: "AdditionalInformation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdditionalInformation",
+                table: "AspNetUsers",
+                newName: "ProfileData");
+        }
+    }
+}
