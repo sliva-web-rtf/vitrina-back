b6ec71590c8b1eec390f0ad01edaece6214ab92b
stepan <stepan.zakolyukin@yandex.ru>
2025-05-10 21:27:24 +0500

    Removed the use of UserRepository

--- src/Vitrina.UseCases/User/GetUserProjectsPages/GetUserProjectPagesByUserIdQueyHandler.cs
+++ src/Vitrina.UseCases/User/GetUserProjectsPages/GetUserProjectPagesByUserIdQueyHandler.cs
@@ -1,18 +1,20 @@
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Saritasa.Tools.Domain.Exceptions;
-using Vitrina.Infrastructure.Abstractions.Interfaces.Repositories;
 using Vitrina.UseCases.ProjectPages;
 
 namespace Vitrina.UseCases.User.GetUserProjectsPages;
 
-public class GetUserProjectPagesByUserIdQueyHandler(IUserRepository userRepository, IMapper mapper)
+/// <inheritdoc />
+public class GetUserProjectPagesByUserIdQueyHandler(UserManager<Domain.User.User> userManager, IMapper mapper)
     : IRequestHandler<GetUserProjectPagesByUserIdQuey, ICollection<ProjectPageDto>>
 {
+    /// <inheritdoc />
     public async Task<ICollection<ProjectPageDto>> Handle(GetUserProjectPagesByUserIdQuey request,
         CancellationToken cancellationToken)
     {
-        var user = await userRepository.GetByIdAsync(request.UserId, cancellationToken) ??
+        var user = await userManager.FindByIdAsync($"{request.UserId}") ??
                    throw new NotFoundException("The user with the specified Id was not found");
 
         return mapper.Map<ICollection<ProjectPageDto>>(user.EditingRights.Select(editor => editor.Page));
