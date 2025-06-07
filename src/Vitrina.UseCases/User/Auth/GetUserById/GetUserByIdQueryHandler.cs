0096e3688a448249997103a2a56bf4082a34680c
stepan <stepan.zakolyukin@yandex.ru>
2025-06-07 22:04:22 +0500

    small fix

--- src/Vitrina.UseCases/User/Auth/GetUserById/GetUserByIdQueryHandler.cs
+++ src/Vitrina.UseCases/User/Auth/GetUserById/GetUserByIdQueryHandler.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using MediatR;
-using Saritasa.Tools.Domain.Exceptions;
 using Saritasa.Tools.EntityFrameworkCore;
 using Vitrina.Infrastructure.Abstractions.Interfaces;
 using Vitrina.UseCases.User.DTO;
@@ -16,14 +15,7 @@ internal class GetUserByIdQueryHandler(IAppDbContext dbContext, IMapper mapper)
     /// <inheritdoc />
     public async Task<UserDetailsDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
     {
-        try
-        {
-            var user = await dbContext.Users.GetAsync(user => user.Id == request.UserId, cancellationToken);
-            return mapper.Map<UserDetailsDto>(user);
-        }
-        catch (Exception ex)
-        {
-            throw new DomainException();
-        }
+        var user = await dbContext.Users.GetAsync(user => user.Id == request.UserId, cancellationToken);
+        return mapper.Map<UserDetailsDto>(user);
     }
 }
