2392cf123e015a2124d7cbbd8058313e12707a68
miroy <miroeg2020@gmail.com>
2025-12-10 22:03:49 +0500

    Добавил Serilog и логирование бизнес логики

--- src/Vitrina.Web/Infrastructure/DependencyInjection/MediatRModule.cs
+++ src/Vitrina.Web/Infrastructure/DependencyInjection/MediatRModule.cs
@@ -20,6 +20,10 @@ internal static class MediatRModule
         services.AddTransient<ISpecializationRepository, SpecializationRepository>();
         services.AddTransient<IProjectPageRepository, ProjectPageRepository>();
         services.AddTransient<IPageEditorRepository, PageEditorRepository>();
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProjectCommand).Assembly));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(CreateProjectCommand).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(MediatRModule).Assembly);
+        });
     }
 }
